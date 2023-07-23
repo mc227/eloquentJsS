a7590fe25511390e0d23de7c53cb8ff19d0e62d4
mc227 <markcostales.mark@gmail.com>
2023-07-23 18:39:25 -0400

    sometimes it just takes longer for others than most

--- squared.js
+++ squared.js
@@ -15,12 +15,80 @@ make that several books will make my work with chatgpt better
 //     return "flying cars"
 // }
 
-const power = (base,exponent) => {
-    let result = 1
-    for(let i = 0; i < exponent; i++) {
-        result = result*base
-    }
-    console.log(result)
-}
+// const power = (base,exponent) => {
+//     let result = 1
+//     for(let i = 0; i < exponent; i++) {
+//         result = result*base
+//     }
+//     console.log(result)
+// }
 
-power(2,3)
+// power(2,3)
+
+/*talking about the single ready to mingle parameter that is by itself*/
+// const square1 = (x) => { return x*x};
+// const square2 = x =>  x*x;
+
+// const horn = () => {
+//     console.log("Toot");
+// }
+
+// const greeting = function(who) {
+//     console.log("Hello" + " " + who)
+// }
+// greeting("Bob")
+// greeting("Bye")
+
+// const chicken = function(){
+//     return egg();
+// }
+
+// const egg = function(){
+//     return chicken();
+// }
+
+// console.log(chicken() + " came first")
+
+// function square(number) {
+//     return number*number
+// }
+// console.log(square(2,"hedgehog","foo"))
+
+// function minus(a,b) {
+//     if (a == undefined) {
+//         return -b
+//     } else {
+//         return a - b;
+//     }
+// }
+
+// console.log(minus(100,9))
+
+// const power = function(base,exponent=2) {
+//     let result = 1
+//     for(let i = 0; i < exponent;i++) {
+//         result = result*base
+//     }
+//     return result
+// }
+// console.log(power(2,6))
+
+// console.log("sisig","hooray","mc")
+
+// function wrapValue(n){
+//     let local = n;
+//     return () => local;
+// }
+
+// let wrap1 = wrapValue(1)
+// console.log(wrap1())
+
+// let wrap2 = wrapValue(2)
+// console.log(wrap2())
+
+// function multiplier(factor) {
+//     return number => number * factor;
+// }
+
+// let twice = multiplier(2);
+// console.log(twice(5))
