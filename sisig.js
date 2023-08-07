cb78d34db464c843d5c4e615119fca2a1b0e17e9
mc227 <markcostales.mark@gmail.com>
2023-08-06 22:22:03 -0400

    i got it

--- sisig.js
+++ sisig.js
@@ -111,4 +111,27 @@ function phi(table) {
 
 // console.log(phi([76,9,4,1]))
 
-console.log(JOURNAL.length)
+// console.log(JOURNAL.length) // 3 month journal
+
+/**
+ * To extract a two-by-two table for a 
+ * specific event from the journal, 
+ * we must loop over all the entries 
+ * and tally how many times the event 
+ * occurs in relation to squirrel 
+ * transformations.
+ */
+
+function tableFor(event,journal) {
+    let table = [0,0,0,0]
+    for(let i = 0; i < journal.length; i++) {
+        let index = 0
+        let entry = journal[i]
+        if (entry.events.includes(event)) index+=1
+        if (entry.squirrel) index+=2
+        table[index] += 1
+    }
+    return table
+}
+
+console.log(tableFor("pizza", JOURNAL))
