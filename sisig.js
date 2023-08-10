bbde4fd56bf3e68cc1b10617576474ac218120c5
mc227 <markcostales.mark@gmail.com>
2023-08-10 19:23:08 -0400

    not yet

--- sisig.js
+++ sisig.js
@@ -581,7 +581,21 @@ function sum(numbers) {
 
 // Your code here.
 
+function deepEqual(a, b) {
+    if (a === b) return true
+    
+    if (a == null || typeof a != "object" || b == null || typeof b != "object") return false
 
+    let keysA = Object.keys(a)
+    let keysB = Object.keys(b)
+
+    if (keysA.length!=keysB.length) return false
+    for(let key of keysA) {
+        if(!keysB.includes(key)|| !deepEqual(a[key],b[key])) return false
+    }
+    return true
+}
+  
 
 let obj = {here: {is: "an"}, object: 2};
 console.log(deepEqual(obj, obj));
