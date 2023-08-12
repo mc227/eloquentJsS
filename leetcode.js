751ea4d0d360a329c9cbca420f2d174ce443feb9
mc227 <markcostales.mark@gmail.com>
2023-08-12 11:46:29 -0400

    closer but still not yet there

--- leetcode.js
+++ leetcode.js
@@ -1,20 +1,30 @@
 var longestCommonPrefix = function(strs) {
-    let first = strs[0]
-    let prefix = first.substring(0,1)
-    for(let i = 1; i<strs.length; i++) {
-        console.log(prefix == strs[i].substring(0,1))
+    // let first = strs[0].substring(0,1)
+    // console.log(first)
+    // let prefix = ""
+    // for(let i = 1; i <  strs.length; i++) {
+    //     if (strs[0].substring(0,1) == strs[i].substring(0,1)) {
+    //         prefix = strs[0].substring(0,1)
+    //     }
+    // }
+    // return prefix
+    var first = strs[0]
+    let prefix = ""
+    console.log("first word: ", first)
+    // loop through the substring of the first word
+    for (let i = 1; i <= first.length; i++) {
+        console.log(first.substring(0,i))
+        for (let j = 1; j < strs.length; j++) {
+            console.log("****************")
+            console.log("comparing ",first.substring(0,i)," with ", strs[j].substring(0,i))
+            if (first.substring(0,i) === strs[j].substring(0,i)) {
+                prefix = first.substring(0,i)
+            }
+            
+        }
+        console.log("prefix: ", prefix)
     }
-    // since it is true, i update prefix
-    prefix = first.substring(0,2)
-    for(let i = 1; i<strs.length; i++) {
-        console.log(prefix == strs[i].substring(0,2))
-    }
-    // since it is true, i update prefix
-    prefix = first.substring(0,3)
-    for(let i = 1; i<strs.length; i++) {
-        console.log(prefix == strs[i].substring(0,3))
-    }
-    // since there is a false
 };
 let words = ["flower","flow","flight"]
 longestCommonPrefix(words)
+// console.log(longestCommonPrefix(words))
