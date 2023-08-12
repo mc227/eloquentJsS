a29842fbbd3a58d7fd74e416a970430d354d1108
mc227 <markcostales.mark@gmail.com>
2023-08-11 20:43:12 -0400

    i was able to do it from scratch without taking notes but i'm not sure what i did

--- sisig.js
+++ sisig.js
@@ -683,4 +683,14 @@ function repeat(n,action) {
     }
 }
 
-repeat(3, console.log)
+// repeat(3, console.log)
+
+function unless(test, then) {
+    if(!test) then()
+}
+
+repeat(3, n=> {
+    unless(n % 2 == 1, () => {
+        console.log(n," is even")
+    })
+})
