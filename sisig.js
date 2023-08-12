91b5f153336a482f39a9b959c70c0195cdc90388
mc227 <markcostales.mark@gmail.com>
2023-08-11 20:49:09 -0400

    chatgpt re wrote it in traditional terms

--- sisig.js
+++ sisig.js
@@ -677,20 +677,3 @@
 
 // noisy(Math.min)(1,2,3)
 
-function repeat(n,action) {
-    for(let i = 0; i < n; i++) {
-        action(i)
-    }
-}
-
-// repeat(3, console.log)
-
-function unless(test, then) {
-    if(!test) then()
-}
-
-repeat(3, n=> {
-    unless(n % 2 == 1, () => {
-        console.log(n," is even")
-    })
-})
