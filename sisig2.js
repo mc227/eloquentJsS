a263fbb96a8eea9c1917a8c4e280e8f1f607c968
mc227 <markcostales.mark@gmail.com>
2023-08-16 01:28:01 -0400

    i had to dig deep for that one

--- sisig2.js
+++ sisig2.js
@@ -1139,4 +1139,18 @@ function repeat(n,action) {
   }
 }
 
-repeat(10,console.log)
+// repeat(10,console.log)
+
+/**
+ * We don’t have to pass a predefined
+ *  function to repeat. Often, 
+ * it is easier to create a 
+ * function value on the spot instead.
+ */
+
+let result = []
+repeat(5, i=>{
+  result.push(`Unit ${i}`)
+})
+
+console.log(result)
