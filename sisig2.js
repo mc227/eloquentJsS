5753f4cba08806796dcce9a6d280045745568d60
mc227 <markcostales.mark@gmail.com>
2023-08-19 22:15:12 -0400

    trying again

--- sisig2.js
+++ sisig2.js
@@ -1388,18 +1388,18 @@ function countBy(items, groupName) {
 // console.log(countBy([1,2,3,4,5], n=>n>2))
 
 // what does codePointAt do
-function textScripts(text) {
-  let scripts = countBy(text, char=>{
-    let script = characterScript(char.codePointAt(0));
-    return script ? script.name:"none"
-  }).filter(({name})=> name!="none")
+// function textScripts(text) {
+//   let scripts = countBy(text, char=>{
+//     let script = characterScript(char.codePointAt(0));
+//     return script ? script.name:"none"
+//   }).filter(({name})=> name!="none")
 
-  let total = scripts.reduce((n,{count})=> n + count, 0)
-  if(total == 0) return "No scripts found"
+//   let total = scripts.reduce((n,{count})=> n + count, 0)
+//   if(total == 0) return "No scripts found"
 
-  return scripts.map(({name,count})=>{
-    return `${Math.round(count*100/total)}% ${name}`;
-  }).join(", ")
-}
+//   return scripts.map(({name,count})=>{
+//     return `${Math.round(count*100/total)}% ${name}`;
+//   }).join(", ")
+// }
 console.log(textScripts('英国的狗说"woof", 俄罗斯的狗说"тяв"'));
 // → 61% Han, 22% Latin, 17% Cyrillic
---
+++ sisig3.js
@@ -0,0 +1,1 @@
+console.log("sisig 3.js")
