ff738b5580c21d9c95a77ce2428ccf85c67bbff1
mc227 <markcostales.mark@gmail.com>
2023-08-19 22:21:09 -0400

    successfully created sum and range functions

--- sisig3.js
+++ sisig3.js
@@ -1,7 +1,29 @@
 // sum 1 to 10
-let count = 1, total = 0
-while(count <=10) {
-    total +=count
-    count++
+// let count = 1, total = 0
+// while(count <=10) {
+//     total +=count
+//     count++
+// }
+// console.log(total)
+// 55
+
+// create sum and range functions that will output 55 for console.log(sum(range(1, 10)));
+
+function sum(array) {
+    let total = 0
+    for(let element of array) {
+        total += element
+    }
+    return total
 }
-console.log(total)
+
+function range(start, end) {
+    let result = []
+    while (start <=end) {
+        result.push(start)
+        start++
+    }
+    return result
+}
+
+console.log(sum(range(1, 10)));
