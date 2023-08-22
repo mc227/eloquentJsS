f8adba0b65367984ea40ffb4d37cccda19292fe1
mc227 <markcostales.mark@gmail.com>
2023-08-22 07:13:27 -0400

    i get it

--- sisig3.js
+++ sisig3.js
@@ -1244,3 +1244,21 @@ function reduce(array, combine, start) {
 
 // console.log([1,2,3,4].reduce((a,b)=>{return a+b}))
 // use reduce (twice) to find the script with the most characters
+
+function characterCount(script) {
+    return script.ranges.reduce((count,[from,to]) => {
+        return count + (to - from)
+    }, 0)
+}
+
+// console.log(SCRIPTS.reduce((a,b)=>{
+//     return characterCount(a) < characterCount(b) ? b : a
+// }))
+
+let biggest = null
+for(let script of SCRIPTS){
+    if (biggest == null || characterCount(biggest) < characterCount(script)){
+        biggest =  characterCount(biggest)
+    }
+}
+console.log(biggest)
