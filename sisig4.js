6359def37aa8eaef7dc7ac99e06e32489bd0f39b
mc227 <markcostales.mark@gmail.com>
2023-09-01 09:46:02 -0400

    once again deleting it so that i can do it from scratch

--- sisig4.js
+++ sisig4.js
@@ -1,11 +1,3 @@
 /*
 This code calculates the sum of integers from 1 to 10 and prints the result, which is 55.
 */
-let count = 1, total = 0
-
-while (count <=10){
-    total+=count
-    count++
-}
-
-console.log(total)
