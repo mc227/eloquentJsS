1c609ebfcd96364d476d002cc0c4d4b2b475ef64
mc227 <markcostales.mark@gmail.com>
2023-09-02 22:31:27 -0400

    i learned it by discovery but a little uncertain

--- sisig4.js
+++ sisig4.js
@@ -62,5 +62,7 @@ We don’t have to pass a predefined function to repeat.
 Often, it is easier to create a function value on the spot instead.
 */
 let labels = []
-repeat(5,n=>labels.push(`Unit ${n+1}`))
+repeat(5,n => {
+    return labels.push(`Unit ${n+1}`)
+})
 console.log(labels)
