5d3e36e79b27aa186d10aa94ef8857932dfe10fa
mc227 <markcostales.mark@gmail.com>
2023-09-02 22:22:16 -0400

    i did it without seeing it

--- sisig4.js
+++ sisig4.js
@@ -28,3 +28,14 @@ function range(start,end) {
 
 // console.log(sum(range(1, 10)));
 
+for(let i = 0; i < 10; i++) {
+    // console.log(i)
+}
+
+function repeatLog(n) {
+    for(let i = 0; i < n; i++) {
+        console.log(i)
+    }
+}
+
+// repeatLog(10)
