d91bdded712553197108e4ef6479881473f305c8
mc227 <markcostales.mark@gmail.com>
2023-09-03 09:41:52 -0400

    insert Magneto Perfection meme

--- sisig4.js
+++ sisig4.js
@@ -62,4 +62,7 @@ We don’t have to pass a predefined function to repeat.
 Often, it is easier to create a function value on the spot instead.
 */
 let labels = []
+repeat(5, i => {
+    labels.push(`Unit ${i+1}`)
+})
 console.log(labels)
