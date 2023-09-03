0fd8c3272030a766a04236ec1b2d9c1441b5141b
mc227 <markcostales.mark@gmail.com>
2023-09-02 22:17:27 -0400

    back at it again

--- sisig4.js
+++ sisig4.js
@@ -7,4 +7,24 @@ while(count <= 10){
     total += count
     count++
 }
-console.log(total)
+// console.log(total)
+
+function sum(array) {
+    let result = 0
+    for(let item of array) {
+        result += item
+    }
+    return result
+}
+
+function range(start,end) {
+    let result = []
+    while(start<=end) {
+        result.push(start)
+        start++
+    }
+    return result
+}
+
+// console.log(sum(range(1, 10)));
+
