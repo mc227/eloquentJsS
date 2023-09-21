a4f363e65f01780b2cf1a1e40a0b5d4c122f9259
mc227 <markcostales.mark@gmail.com>
2023-09-21 18:19:27 -0400

    this one with a for loop

--- sisig6.js
+++ sisig6.js
@@ -1,1 +1,12 @@
-console.log("Oh hi Mark")
+/**
+ * I'm writing a for loop with sum and total.
+ * I'm also supposed to use count and 
+ * I'm supposed to console.log in the end
+ */
+
+let total = 0, count = 1
+for(let i = 0; i < 10; i++) {
+    total+=count
+    count++
+}
+console.log(total)
