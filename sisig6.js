4c6f6b8e7c4a96ac39e3c3bcfe677cc82a63d488
mc227 <markcostales.mark@gmail.com>
2023-09-21 18:29:29 -0400

    i missed the return statement

--- sisig6.js
+++ sisig6.js
@@ -4,9 +4,41 @@
  * I'm supposed to console.log in the end
  */
 
-let total = 0, count = 1
-for(let i = 0; i < 10; i++) {
-    total+=count
-    count++
+// let total = 0, count = 1
+// for(let i = 0; i < 10; i++) {
+//     total+=count
+//     count++
+// }
+// console.log(total)
+
+// what about a while loop
+
+// let total = 0, count = 1
+
+// while(count <= 10) {
+//     total += count
+//     count++
+// }
+
+// console.log(total)
+
+// I'm suppsed to have this work
+
+function sum(array) {
+    let result = 0
+    for(let item of array){
+        result+=item
+    }
+    return result
 }
-console.log(total)
+
+function range(start, end) {
+    let result = []
+    while(start <= end) {
+        result.push(start)
+        start+=1
+    }
+    return result
+}
+
+console.log(sum(range(1, 10)));
