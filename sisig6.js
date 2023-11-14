1ad8cb203693343f2bab994cff9ce2bae7f9cfba
mc227 <markcostales.mark@gmail.com>
2023-11-14 14:20:48 -0500

    passed flattening exercise

--- sisig6.js
+++ sisig6.js
@@ -275,6 +275,16 @@ function textScripts(text) {
 // → 61% Han, 22% Latin, 17% Cyrillic
 // console.log(textScripts(''));
 
+
+// Use the reduce method in combination 
+// with the concat method to “flatten” 
+// an array of arrays into a single 
+// array that has all the elements of the original arrays.
+
 let arrays = [[1, 2, 3], [4, 5], [6]];
 
-console.log(arrays.reduce((flat,current)=>flat.concat(current),[]))
+console.log(
+    arrays.reduce(
+        (flat,current)=>flat.concat(current),[]
+        )
+    )
