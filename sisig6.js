c50d29c36ea1e2fa5b274ef6f283f97ae925e4a0
mc227 <markcostales.mark@gmail.com>
2023-11-20 05:51:22 -0500

    doing by discovery the dominant writing direction

--- sisig6.js
+++ sisig6.js
@@ -290,12 +290,45 @@ let arrays = [[1, 2, 3], [4, 5], [6]];
 //     )
 
 
-// Your code here.
+// // Your code here.
 
+// function loop(start, test, update, body){
+//     for(let value = start; test(value); value = update(value)){
+//         body(value)
+//     }
+// }
 
+// loop(3, n => n > 0, n => n - 1, console.log);
+// // → 3
+// // → 2
+// // → 1
 
+// function every(array, test){
+//     for(let item of array){
+//         if(!test(item)) {
+//             return false
+//         }
+//     }
+//     return true
+// }
 
-loop(3, n => n > 0, n => n - 1, console.log);
-// → 3
-// → 2
-// → 1
+// console.log(every([1, 3, 5], n => n < 10));
+// // → true
+// console.log(every([2, 4, 16], n => n < 10));
+// // → false
+// console.log(every([], n => n < 10));
+// // → true
+
+function dominantDirection(text) {
+    // Your code here.
+    let scripts = countBy(text, char => {
+        let script = characterScript(char.codePointAt(0));
+        return script ? script.name : "none";
+    }).filter(({name}) => name != "none");
+    console.log(scripts)
+}
+  
+console.log(dominantDirection("Hello!"));
+// → ltr
+console.log(dominantDirection("Hey, مساء الخير"));
+// → rtl
