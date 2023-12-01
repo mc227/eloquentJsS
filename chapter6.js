facc40ec0993cbaaf643e86f5e5c0ca44cdf19a0
mc227 <markcostales.mark@gmail.com>
2023-11-30 22:01:54 -0500

    nailed it

--- chapter6.js
+++ chapter6.js
@@ -1,18 +1,9 @@
-// let rabbit = {}
-
-// rabbit.speak = function(line) {
-//     console.log(`The rabbit says ${line}`)
-// }
-
-// rabbit.speak("oh hi Mark")
-
 function speak(line) {
-    console.log(`The ${this.type} rabbit says '${line}'`);
+    console.log(`The ${this.type} rabbit says, '${line}'`)
 }
 
-let whiteRabbit = {type: "white", speak}
-let hungryRabbit = {type: "hungry", speak};
+let whiteRabbit = {type:"white", speak}
+let hungryRabbit = {type:"hungry", speak}
 
-// whiteRabbit.speak("Oh my ears and whiskers, " + "how late it's getting!");
-
-hungryRabbit.speak("I could use a carrot right now.")
+whiteRabbit.speak("oh hi Mark")
+hungryRabbit.speak("Oh Hi Jhonny")
