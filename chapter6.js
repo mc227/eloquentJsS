8456c315b66fef69dfd74aa0afbecef4e1849a39
mc227 <markcostales.mark@gmail.com>
2023-12-01 19:19:49 -0500

    more complete

--- chapter6.js
+++ chapter6.js
@@ -16,4 +16,9 @@ function normalize(){
     console.log(this.coords.map(n => n/ this.length))
 }
 
-normalize.call({coords:[0,2,3],length:5})
+// normalize.call({coords:[0,2,3],length:5})
+// let empty = {}
+// console.log(empty.toString)
+// console.log(empty.toString())
+
+console.log(Object.getPrototypeOf({})== Object.prototype)
