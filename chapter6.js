4564cf973f6bd54491710cd1bc0a21f88db6338c
mc227 <markcostales.mark@gmail.com>
2023-12-01 19:00:29 -0500

    before magic

--- chapter6.js
+++ chapter6.js
@@ -5,5 +5,15 @@ function speak(line) {
 let whiteRabbit = {type:"white", speak}
 let hungryRabbit = {type:"hungry", speak}
 
-whiteRabbit.speak("oh hi Mark")
-hungryRabbit.speak("Oh Hi Jhonny")
+// whiteRabbit.speak("oh hi Mark")
+// hungryRabbit.speak("Oh Hi Jhonny")
+
+
+
+// speak.call(whiteRabbit,"Oh Hi Mark")
+
+function normalize(){
+    console.log(this.coords.map(n => n/ this.length))
+}
+
+normalize.call({coords:[0,2,3],length:5})
