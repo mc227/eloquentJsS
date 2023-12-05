5889a5994c0a567041faa9c4b5e7280eff187456
mc227 <markcostales.mark@gmail.com>
2023-12-04 23:56:45 -0500

    i have to do this again

--- chapter6.js
+++ chapter6.js
@@ -1,15 +1,9 @@
-let protoRabbit = {
-    speak(line){
-        console.log(`The ${this.type} rabbit says '${line}'`)
-    }
+function Rabbit(type){
+    this.type = type;
 }
 
-// let killerRabbit = Object.create(protoRabbit);
-// killerRabbit.type = "killer"
-// killerRabbit.speak("SKREEE")
-
-function makeRabbit(type){
-    let rabbit = Object.create(protoRabbit);
-    rabbit.type = type;
-    return rabbit;
+Rabbit.prototype.speak = function(line){
+    console.log(`The ${this.type} rabbit says'${line}'`)
 }
+
+let weirdRabbit = new Rabbit("weird")
