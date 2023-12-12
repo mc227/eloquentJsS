d0f46ee27e3da7247e24fb1a68ddcd52006ac08a
mc227 <markcostales.mark@gmail.com>
2023-12-12 00:00:11 -0500

    another day another dollar

--- chapter6.js
+++ chapter6.js
@@ -1,9 +1,24 @@
-function Rabbit(type){
-    this.type = type;
+// function Rabbit(type){
+//     this.type = type;
+// }
+
+// Rabbit.prototype.speak = function(line) {
+//     console.log(`The ${this.type} rabbit says '${line}'`);
+// }
+
+// let weirdRabbit = new Rabbit("weird")
+
+// console.log(Object.getPrototypeOf(Rabbit) == Function.prototype)
+// console.log(Object.getPrototypeOf(weirdRabbit) === Rabbit.prototype)
+
+class Rabbit {
+    constructor(type){
+        this.type = type
+    }
+    speak(line) {
+        console.log(`The ${this.type} rabbit says '${line}'`)
+    }
 }
 
-Rabbit.prototype.speak = function(line) {
-    console.log(`The ${this.type} rabbit says '${line}'`);
-}
-
+let whiteRabbit = new Rabbit("white")
 let weirdRabbit = new Rabbit("weird")
