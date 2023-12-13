1e1cbbd2cc052f27876752abf21f71d889c202df
mc227 <markcostales.mark@gmail.com>
2023-12-12 21:31:33 -0500

    proceed

--- chapter6.js
+++ chapter6.js
@@ -1,24 +1,16 @@
-// function Rabbit(type){
-//     this.type = type;
-// }
-
-// Rabbit.prototype.speak = function(line) {
-//     console.log(`The ${this.type} rabbit says '${line}'`);
-// }
-
-// let weirdRabbit = new Rabbit("weird")
-
-// console.log(Object.getPrototypeOf(Rabbit) == Function.prototype)
-// console.log(Object.getPrototypeOf(weirdRabbit) === Rabbit.prototype)
-
-class Rabbit {
+class Rabbit{
     constructor(type){
         this.type = type
     }
-    speak(line) {
+    speak(line){
         console.log(`The ${this.type} rabbit says '${line}'`)
     }
 }
 
-let whiteRabbit = new Rabbit("white")
-let weirdRabbit = new Rabbit("weird")
+let object = new class{
+    getWord(){
+        return "hello"
+    }
+}
+
+// console.log(object.getWord())
