76301fc5fcfd8e9e0704a6bf605e68ec6927d0b4
mc227 <markcostales.mark@gmail.com>
2023-12-14 11:42:19 -0500

    clear

--- chapter6.js
+++
@@ -1,9 +0,0 @@
-let age = {
-    Julia:62,
-    Boris:62,
-    Liang:62
-}
-
-console.log(`Julia is ${age.Julia}`)
-console.log("Is Jack's age known? ", "Jack" in age)
-console.log("Is toString's age known? ", "toString" in age)
