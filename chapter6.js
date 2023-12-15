385259fa74955caac3e91884b12ba21375ac48a9
mc227 <markcostales.mark@gmail.com>
2023-12-15 12:45:53 -0500

    you got it

--- chapter6.js
+++ chapter6.js
@@ -1,8 +1,2 @@
-let map = new Map();
-map.set("Julia",62)
-map.set("Boris",62)
-map.set("Liang",62)
-
-console.log(`Julia is ${map.get("Julia")}`)
-console.log("Does map have Jack? ", map.has("Jack"))
-console.log("Does map have toString? ", map.has("toString"))
+console.log({x:1}.hasOwnProperty("x"))
+console.log({x:1}.hasOwnProperty("toString"))
