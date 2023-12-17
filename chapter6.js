221a19fdf919150d1204d87c3c6bc413d35eb441
mc227 <markcostales.mark@gmail.com>
2023-12-16 23:43:09 -0500

    you did it. proceed

--- chapter6.js
+++ chapter6.js
@@ -1,2 +1,18 @@
-console.log({x:1}.hasOwnProperty("x"))
-console.log({x:1}.hasOwnProperty("toString"))
+class Rabbit{
+    constructor(type){
+        this.type = type
+    }
+    
+    speak(line){
+        console.log(`The ${this.type} rabbit says '${line}'`)
+    }
+}
+
+whiteRabbit = new Rabbit("White")
+console.log(whiteRabbit.toString())
+
+Rabbit.prototype.toString = function() {
+    return `a ${this.type} Rabbit`
+}
+
+console.log(whiteRabbit.toString())
