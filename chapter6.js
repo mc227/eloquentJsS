be46306c7564a25d57999e1323d988274a894ca3
mc227 <markcostales.mark@gmail.com>
2023-12-18 00:07:25 -0500

    proceed

--- chapter6.js
+++ chapter6.js
@@ -1,18 +1,15 @@
-class Rabbit{
-    constructor(type){
-        this.type = type
+class Rabbit {
+    constructor(type) {
+      this.type = type;
     }
-    
-    speak(line){
-        console.log(`The ${this.type} rabbit says '${line}'`)
+    speak(line) {
+      console.log(`The ${this.type} rabbit says '${line}'`);
     }
-}
+  }
 
-whiteRabbit = new Rabbit("White")
-console.log(whiteRabbit.toString())
+let blackRabbit = new Rabbit('black')
 
-Rabbit.prototype.toString = function() {
-    return `a ${this.type} Rabbit`
-}
-
-console.log(whiteRabbit.toString())
+let sym = Symbol("name")
+console.log(sym == Symbol("name"))
+Rabbit.prototype[sym] = 55
+console.log(blackRabbit[sym])
