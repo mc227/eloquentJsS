4cac7a8449df353349c2fb45475d56afcb6a9272
mc227 <markcostales.mark@gmail.com>
2023-12-29 06:19:46 -0500

    my little tdd

---
+++ sumRangeScript.js
@@ -0,0 +1,16 @@
+/*
+   i asked chat GPT for a good name for this javascript file. 
+   This was my prompt:
+   what should i name a javascript file that runs this
+    console.log(sum(range(1, 10)));
+    // → 55
+
+    i still have to write my own sum and range functions in this same file
+
+    
+    ChatGPT:
+    You might name the file something like "sumRangeScript.js" to reflect 
+    that it contains a script involving the sum and range functions
+*/
+
+console.log(sum(range(1, 10)));
